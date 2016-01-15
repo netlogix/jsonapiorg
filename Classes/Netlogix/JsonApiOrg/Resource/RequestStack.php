2f05d319a212693a69a8a082f22a191c344da5ae
Stephan Schuler <stephan.schuler@netlogix.de>
2016-01-15 15:29:50 +0100

    Add a stack holding future and past requests

---
+++ Classes/Netlogix/JsonApiOrg/Resource/RequestStack.php
@@ -0,0 +1,90 @@
+<?php
+namespace Netlogix\JsonApiOrg\Resource;
+
+/*
+ * This file is part of the Netlogix.JsonApiOrg package.
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Just a simple stack that knows about current, future and past
+ * requests.
+ */
+class RequestStack {
+
+	const POSITION_DATA = 'data';
+	const POSITION_DATACOLLECTION = 'data[]';
+	const POSITION_INCLUDE = 'include';
+
+	const RESULT_URI = 'uri';
+	const RESULT_POSITION = 'position';
+	const RESULT_DATA = 'data';
+	const RESULT_DATA_IDENTIFIER = 'id';
+	const RESULT_NESTING_PATHS = 'nestingPaths';
+
+	protected $open = array();
+
+	protected $results = array();
+
+	/**
+	 * @param string $uri
+	 * @param array $dataIdentifier
+	 * @param string $position
+	 * @param string $nestingPath
+	 */
+	public function push($uri, array $dataIdentifier, $position = self::POSITION_DATA, $nestingPath = '') {
+		if (array_key_exists($uri, $this->results)) {
+			$this->results[$uri][self::RESULT_NESTING_PATHS][$nestingPath] = $nestingPath;
+			return;
+		}
+
+		$this->results[$uri] = array(
+			self::RESULT_URI => $uri,
+			self::RESULT_POSITION => $position,
+			self::RESULT_DATA_IDENTIFIER => $dataIdentifier,
+			self::RESULT_DATA => null,
+			self::RESULT_NESTING_PATHS => array($nestingPath => $nestingPath)
+		);
+		$this->open[] = $uri;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function pop() {
+		if (!count($this->open)) {
+			return NULL;
+		}
+		$uri = array_pop($this->open);
+		return $this->results[$uri];
+	}
+
+	/**
+	 * @param $uri
+	 * @param \Netlogix\JsonApiOrg\Schema\Resource $content
+	 */
+	public function finalize($uri, \Netlogix\JsonApiOrg\Schema\Resource $content) {
+		$this->results[$uri][self::RESULT_DATA] = $content;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getResults() {
+		return $this->results;
+	}
+
+	/**
+	 * @param $uri
+	 * @return mixed
+	 */
+	public function getNestingPaths($uri) {
+		return $this->results[$uri][self::RESULT_NESTING_PATHS];
+	}
+
+}
